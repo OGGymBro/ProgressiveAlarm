3a7ae78773dd37be875ddcffe5f71ee54964fd60
OGGymBro <143493367+OGGymBro@users.noreply.github.com>
2024-04-21 16:07:00 +0530

    we arent going past the desired amount now ..its fixed

--- ProgressiveAlarm/AlarmSetter.swift
+++ ProgressiveAlarm/AlarmSetter.swift
@@ -94,7 +94,7 @@ struct ContentView: View {
         
         // Schedule notifications for each day
         var currentDate = startDate
-        for _ in 0...numberOfDays {
+        for _ in 0..<numberOfDays {
             scheduleNotification(for: currentDate, sleepTime: currentSleepTime, wakeTime: currentWakeTime)
             
             // Increment date and times
